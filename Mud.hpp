ef10c38a9620b8004ff812b93da1fa5f2ecc5613
lassyla <a.alyssa.lee@gmail.com>
2020-12-03 00:09:55 -0500

    added acorns and one mushroom level

--- Mud.hpp
+++ Mud.hpp
@@ -8,9 +8,10 @@
 struct Mud : BgTile {
   // Constructors (inherited)
   using BgTile::BgTile;
-  
+
   // Methods
   void load_models(Scene* scene) override;
   Mud* clone_lightweight(Cell* new_cell) override;
+	void when_fg_obj_moved_into(FgObj& objBeingMoved, const glm::ivec2& displ) override;
 
 };
