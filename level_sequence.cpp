7c51c68052bc6940b3feb638a04b44bba5fdf38c
Dmcdominic <dmcdominic@gmail.com>
2020-12-21 14:37:33 -0500

    Made the last level slightly harder

--- level_sequence.cpp
+++ level_sequence.cpp
@@ -25,7 +25,7 @@ std::vector<level> level_sequence = std::vector<level>({
     //level("T1", glm::uvec2(9, 9), 1),
     level("T3", glm::uvec2(8, 8), 1),
     level("T4", glm::uvec2(8, 8), 1),
-    level("T2", glm::uvec2(9, 10), 1),
+    level("T2", glm::uvec2(8, 10), 1),
   });
 
 
